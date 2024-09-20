7234c77e4ead16587f70766a0b29da1859b1ce42
Fovir <fovir@disroot.org>
2024-09-20 14:22:52 +0800

    add anti_insert_sort.cpp

--- insert_sort/Practice/2.1-2/anti_insert_sort.cpp
+++ insert_sort/Practice/2.1-2/anti_insert_sort.cpp
@@ -2,9 +2,28 @@
 #include<vector>
 #include<cstdlib>
 #include<algorithm>
+#include<ctime>
 
 template<typename T>
 void anti_insert_sort(std::vector<T> & v);
+
+int generateData() { return std::rand() % 20; }
+
+int main(void)
+{
+    const int N = 5;
+
+    std::srand(std::time(0));
+    std::vector<int> v(N);
+
+    std::generate(v.begin(), v.end(), generateData);
+    anti_insert_sort(v);
+
+    for (auto x : v)
+        std::cout << x << " ";
+
+    return 0;
+}
 
 template<typename T>
 void anti_insert_sort(std::vector<T> & v)
@@ -13,6 +32,15 @@ void anti_insert_sort(std::vector<T> & v)
 
     for (int i = 1; i < n; i++)
     {
-        T key = v[i];
+        T current = v[i];
+        int j = i - 1;
+        while (j >= 0 && v[j] < current)
+        {
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = current;
     }
+
+    return;
 }
