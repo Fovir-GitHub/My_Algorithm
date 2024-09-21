1a1a409ed6557f98e3188b8e9b9808b3371275d9
Fovir <fovir@disroot.org>
2024-09-21 15:59:10 +0800

    add binary_search.cpp

---
+++ My_Sort/merge_sort/Practice/binary_search/bin_search.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<iterator>
+
+typedef std::vector<int>::iterator Iter;
+
+bool haveNumber(const std::vector<int> & v, Iter left, Iter right, const int & target);
+
+int main(void)
+{
+    const int VECTOR_SIZE = 1000;
+
+    int number_to_find;
+    std::vector<int> numbers(VECTOR_SIZE);
+
+    for (int i = 0; i < VECTOR_SIZE; i++)
+        numbers[i] = i * i;
+
+    std::cout << "Enter the number you want to find: ";
+
+    while (std::cin >> number_to_find)
+        std::cout << (haveNumber(numbers, numbers.begin(),
+            numbers.end(), number_to_find) ? "Have" : "Don't have")
+        << " " << number_to_find << '\n'
+        << "Enter the next number you want to find: ";
+
+    std::cout << "Done." << '\n';
+
+    return 0;
+}
+
+bool haveNumber(const std::vector<int> & v, Iter left, Iter right, const int & target)
+{
+    int distance;
+    if ((distance = std::distance(left, right)) <= 1)
+        return false;
+
+    Iter middle = left;
+    std::advance(middle, distance / 2);
+
+    if (*middle == target)
+        return true;
+    else if (*middle < target)
+        return haveNumber(v, middle, right, target);
+    else
+        return haveNumber(v, left, middle, target);
+}
