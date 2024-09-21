69c4eb58e41589e2fa38f1b14619ab43dc566ce9
Fovir <fovir@disroot.org>
2024-09-21 16:39:25 +0800

    add majorityElement

---
+++ LeetCode/169. Majority Element/majority_element.cpp
@@ -0,0 +1,26 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> & nums)
+    {
+        int count = 0;
+        int candidate = 0;
+
+        for (auto x : nums)
+        {
+            if (count == 0)
+                candidate = x;
+
+            if (x == candidate)
+                count++;
+            else
+                count--;
+        }
+
+        return candidate;
+    }
+};
