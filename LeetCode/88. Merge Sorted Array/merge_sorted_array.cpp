3fbe53153749b2332a1fb4769ef69fcef2151dda
Fovir <fovir@disroot.org>
2024-09-21 16:18:41 +0800

    finish merge_sorted_array

---
+++ LeetCode/88. Merge Sorted Array/merge_sorted_array.cpp
@@ -0,0 +1,43 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    void merge(vector<int> & nums1, int m, vector<int> & nums2, int n)
+    {
+        vector<int> temp(m + n);
+        int num1_index = 0, num2_index = 0;
+        int i = 0;
+
+        while (num1_index < m && num2_index < n)
+        {
+            if (nums1[num1_index] < nums2[num2_index])
+            {
+                temp[i] = nums1[num1_index];
+                num1_index++;
+            }
+            else
+            {
+                temp[i] = nums2[num2_index];
+                num2_index++;
+            }
+            i++;
+        }
+        while (num1_index < m)
+        {
+            temp[i] = nums1[num1_index];
+            i++;
+            num1_index++;
+        }
+        while (num2_index < n)
+        {
+            temp[i] = nums2[num2_index];
+            i++;
+            num2_index++;
+        }
+
+        copy(temp.begin(), temp.end(), nums1.begin());
+    }
+};
