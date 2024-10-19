a84f8f9b1f2ee86b78ba1b877a2e4ba7858a7bd4
Fovir <fovir@disroot.org>
2024-10-19 16:51:47 +0800

    add linklist

---
+++ Data_Structure/LinkList/linklist.hpp
@@ -0,0 +1,143 @@
+#ifndef _LINKLIST_H_
+#define _LINKLIST_H_
+
+template<typename T>
+class LinkList
+{
+private:
+    struct Node
+    {
+        T value;
+        Node * next;
+    };
+
+    Node * root; /* The root of the link list */
+    Node * now;  /* The last Node position */
+    unsigned int node_count;
+
+public:
+    LinkList();
+    ~LinkList();
+
+    bool push(T push_value);
+    bool insert(T insert_value, unsigned int target_position);
+    bool remove(unsigned int remove_position);
+    bool find(T target);
+
+    void access(void(*func)(T));
+    int size() { return node_count; }
+};
+
+template<typename T>
+LinkList<T>::LinkList()
+{
+    root = new Node;
+    root->next = nullptr;
+    now = root;
+    node_count = 0;
+}
+
+template <typename T>
+LinkList<T>::~LinkList()
+{
+    Node * backup;
+
+    while (root)
+    {
+        backup = root->next;
+        delete root;
+        root = backup;
+    }
+    node_count = 0;
+}
+
+template<typename T>
+bool LinkList<T>::push(T push_value)
+{
+    Node * push_node = new Node;
+    if (!push_node)
+        return false;
+
+    push_node->next = nullptr;
+    push_node->value = push_value;
+
+    if (root->next == nullptr)  /* No element */
+        root->next = push_node;
+    else
+        now->next = push_node;
+
+    now = push_node;
+    ++node_count;
+
+    return true;
+}
+
+template<typename T>
+bool LinkList<T>::insert(T insert_value, unsigned int target_position)
+{
+    target_position = std::min(target_position, node_count);
+    unsigned int position_count = 0;
+    Node * current = root;
+
+    while (position_count++ != target_position)
+        current = current->next;
+
+    Node * insert_node = new Node;
+    if (!insert_node)
+        return false;
+
+    insert_node->value = insert_value;
+    insert_node->next = current->next;
+    current->next = insert_node;
+    ++node_count;
+
+    return true;
+}
+
+template<typename T>
+bool LinkList<T>::remove(unsigned int remove_position)
+{
+    remove_position = std::min(remove_position, node_count - 1);
+
+    Node * current = root;
+    unsigned int position_count = 0;
+
+    while (position_count++ != remove_position)
+        current = current->next;
+
+    if (!current)
+        return false;
+
+    Node * backup_next = current->next;
+    current->next = backup_next->next;
+
+    delete backup_next;
+    --node_count;
+
+    return true;
+}
+
+template<typename T>
+bool LinkList<T>::find(T target)
+{
+    Node * temp = root;
+
+    while (temp = temp->next)
+        if (temp->value == target)
+            return true;
+
+    return false;
+}
+
+template<typename T>
+void LinkList<T>::access(void(*func)(T))
+{
+    Node * temp = root;
+
+    while (temp = temp->next)
+        func(temp->value);
+
+    return;
+}
+
+#endif // !_LINKLIST_H_
---
+++ Data_Structure/LinkList/main.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include"linklist.hpp"
+
+void show(int num);
+void show(LinkList<int> & ll);
+
+int main(void)
+{
+    std::srand(std::time(0));
+
+    const int N = 10, LIMIT = 20;
+    LinkList<int> link_list;
+
+    for (int i = 0; i < N; i++)
+        link_list.push((std::rand() % LIMIT));
+
+    std::cout << "Original:\n";
+    show(link_list);
+
+    std::cout << "After insert():\n";
+    link_list.insert(100, 1);
+    show(link_list);
+
+    std::cout << "After remove():\n";
+    link_list.remove(1);
+    show(link_list);
+
+    std::cout << "After second remove():\n";
+    link_list.remove(100);
+    link_list.remove(-1);
+    show(link_list);
+
+    int find_number;
+    std::cout << "Please enter the find_number: ";
+    std::cin >> find_number;
+    std::cout << (link_list.find(find_number) ? "Find" : "Don't find")
+        << " " << find_number << '\n';
+
+    return 0;
+}
+
+void show(int num)
+{
+    std::cout << num << ' ';
+    return;
+}
+
+void show(LinkList<int> & ll)
+{
+    std::cout << "size: " << ll.size() << '\n';
+    ll.access(show);
+    std::cout << '\n';
+
+    return;
+}
