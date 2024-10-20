c77fc128a42313b01c9816d40753d499efa2d078
Fovir <fovir@disroot.org>
2024-10-20 14:01:36 +0800

    add myqueue

---
+++ Data_Structure/Queue/main.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include"myqueue.hpp"
+
+int main(void)
+{
+    std::srand(std::time(0));
+
+    const int N = 10, LIMIT = 20;
+
+    MyQueue<int> mq;
+
+    std::cout << "Size: " << mq.size() << '\n';
+
+    for (int i = 0; i < N; i++)
+    {
+        int num = std::rand() % LIMIT;
+        std::cout << num << ' ';
+        mq.push(num);
+    }
+    std::cout << '\n';
+
+    std::cout << "Size: " << mq.size() << '\n'
+        << "peek(): " << mq.peek() << '\n';
+
+    std::cout << "After pop():\n";
+    mq.pop();
+    std::cout << "Size: " << mq.size() << '\n'
+        << "peek(): " << mq.peek() << '\n';
+
+    while (!mq.empty())
+    {
+        std::cout << mq.peek() << ' ';
+        mq.pop();
+    }
+    std::cout << '\n';
+
+    std::cout << (mq.pop() ? "success" : "fail") << '\n';
+
+    return 0;
+}
---
+++ Data_Structure/Queue/myqueue.hpp
@@ -0,0 +1,83 @@
+#ifndef _MYQUEUE_HPP_
+#define _MYQUEUE_HPP_
+
+template<typename T>
+class MyQueue
+{
+private:
+    struct Node
+    {
+        Node * next;
+        T value;
+    };
+
+    Node * root;
+    Node * tail;
+    unsigned int node_count;
+
+public:
+    MyQueue();
+    ~MyQueue();
+
+    bool push(T push_value);
+    bool pop();
+    bool empty() { return !node_count; }
+    T peek() { return !empty() ? root->next->value : (T) 0; }
+    unsigned int size() { return node_count; }
+};
+
+template<typename T>
+MyQueue<T>::MyQueue() :node_count(0)
+{
+    root = new Node;
+    root->next = nullptr;
+    tail = root;
+}
+
+template <typename T>
+MyQueue<T>::~MyQueue()
+{
+    while (root)
+    {
+        tail = root->next;
+        delete root;
+        root = tail;
+    }
+}
+
+template<typename T>
+bool MyQueue<T>::push(T push_value)
+{
+    Node * push_node = new Node;
+    if (!push_node)
+        return false;
+
+    push_node->value = push_value;
+    push_node->next = nullptr;
+
+    if (!root->next)
+        root->next = push_node;
+    else
+        tail->next = push_node;
+
+    tail = push_node;
+    ++node_count;
+
+    return true;
+}
+
+template<typename T>
+bool MyQueue<T>::pop()
+{
+    if (empty())
+        return false;
+
+    Node * backup_node = root->next;
+    root->next = backup_node->next;
+    delete backup_node;
+    --node_count;
+
+    return true;
+}
+
+#endif // !_MYQUEUE_HPP_
