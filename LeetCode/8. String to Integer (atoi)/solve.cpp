0f96784665bfe0f56e389be4bb124ebb9c42154c
Fovir <fovir@disroot.org>
2024-11-23 15:24:31 +0800

    solve problem 8

---
+++ LeetCode/8. String to Integer (atoi)/solve.cpp
@@ -0,0 +1,59 @@
+#include<string>
+#include<iostream>
+#include<cctype>
+#include<climits>
+using namespace std;
+
+#define DEBUG cout<<"DEBUG"<<'\n'
+
+class Solution
+{
+public:
+    int myAtoi(string s)
+    {
+        int length = s.length();
+        if (length == 0)
+            return 0;
+
+        long result = 0;
+        int start_position = 0;
+
+        // remove white space
+        while (start_position < length && s[start_position] == ' ')
+            start_position++;
+
+        if (!isdigit(s[start_position]) && s[start_position] != '-' && s[start_position] != '+')
+            return 0;
+
+        bool negative_number = s[start_position] == '-';
+
+        for (int i = (negative_number || s[start_position] == '+' ? start_position + 1 : start_position); i < length; i++)
+        {
+            if (!isdigit(s[i]))
+                break;
+
+            result = result * 10 + s[i] - '0';
+
+            if (negative_number)
+            {
+                if (-result < INT_MIN)
+                    return INT_MIN;
+            }
+            else
+                if (result > INT_MAX)
+                    return INT_MAX;
+        }
+
+        return negative_number ? -result : result;
+    }
+};
+
+int main(void)
+{
+    Solution sol;
+    string input;
+    while (cin >> input)
+        cout << sol.myAtoi(input) << '\n';
+
+    return 0;
+}
