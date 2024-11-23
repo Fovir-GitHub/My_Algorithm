1934c2f74516685550873d41a6e5473cd99328b6
Fovir <fovir@disroot.org>
2024-11-23 14:48:06 +0800

    solve problem 7

---
+++ LeetCode/7. Reverse Integer/solve.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<climits>
+
+class Solution
+{
+public:
+    int reverse(int x)
+    {
+        int result = 0;
+
+        while (x)
+        {
+            if ((result > INT_MAX / 10) || (result < INT_MIN / 10))
+                return 0;
+
+            result = result * 10 + (x % 10);
+            x /= 10;
+        }
+
+        return result;
+    }
+};
+
+int main(void)
+{
+    int input = 0;
+    Solution sol;
+
+    while (std::cin >> input)
+        std::cout << sol.reverse(input) << '\n';
+
+    return 0;
+}
