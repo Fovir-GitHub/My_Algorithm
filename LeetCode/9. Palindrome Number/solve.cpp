b76a0d465b1aafb55b1e92ce3c665340c30f0077
Fovir <fovir@disroot.org>
2024-11-23 15:43:26 +0800

    add method 2

--- LeetCode/9. Palindrome Number/solve.cpp
+++ LeetCode/9. Palindrome Number/solve.cpp
@@ -6,25 +6,42 @@ using namespace std;
 class Solution
 {
 public:
+    // bool isPalindrome(int x)
+    // {
+    //     if (x < 0)
+    //         return false;
+
+    //     string number_string("");
+    //     while (x)
+    //     {
+    //         number_string += x % 10 + '0';
+    //         x /= 10;
+    //     }
+
+    //     int length = number_string.length();
+
+    //     for (int i = 0; i < (length & 1 ? length / 2 + 1 : length / 2); i++)
+    //         if (number_string[i] != number_string[length - i - 1])
+    //             return false;
+
+    //     return true;
+    // }
+
     bool isPalindrome(int x)
     {
         if (x < 0)
             return false;
 
-        string number_string("");
-        while (x)
+        int reverse_number = 0;
+        int copy_number = x;
+
+        while (copy_number)
         {
-            number_string += x % 10 + '0';
-            x /= 10;
+            reverse_number = reverse_number * 10 + copy_number % 10;
+            copy_number /= 10;
         }
 
-        int length = number_string.length();
-
-        for (int i = 0; i < (length & 1 ? length / 2 + 1 : length / 2); i++)
-            if (number_string[i] != number_string[length - i - 1])
-                return false;
-
-        return true;
+        return reverse_number == x;
     }
 };
 
@@ -35,4 +52,6 @@ int main(void)
 
     while (cin >> n)
         cout << sol.isPalindrome(n) << '\n';
+
+    return 0;
 }
