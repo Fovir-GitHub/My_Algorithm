bc997b7382ec7c63053d468ae6649caf4a33bd46
Fovir <fovir@disroot.org>
2025-01-11 17:43:59 +0800

    solve problem 28

---
+++ LeetCode/28.find-the-index-of-the-first-occurrence-in-a-string/28.find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=28 lang=cpp
+ *
+ * [28] Find the Index of the First Occurrence in a String
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    int strStr(string haystack, string needle)
+    {
+        auto result = haystack.find(needle);
+
+        return result == string::npos ? -1 : result;
+    }
+};
+// @lc code=end
+
+int main(void)
+{
+    Solution sol;
+    string hay, needle;
+
+    cin >> hay >> needle;
+    cout << sol.strStr(hay, needle) << '\n';
+
+    return 0;
+}
