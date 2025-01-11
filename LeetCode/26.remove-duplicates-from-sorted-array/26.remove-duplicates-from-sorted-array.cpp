321c0398e9ad9e0eb63b03195d3d0846c48b107e
Fovir <fovir@disroot.org>
2025-01-11 17:35:16 +0800

    solve problem 26

---
+++ LeetCode/26.remove-duplicates-from-sorted-array/26.remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=26 lang=cpp
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> & nums)
+    {
+        auto last = unique(nums.begin(), nums.end());
+        nums.erase(last, nums.end());
+
+        return nums.size();
+    }
+};
+// @lc code=end
+
+int main(void)
+{
+    Solution sol;
+    vector<int> number = {1, 1, 2};
+
+    cout << sol.removeDuplicates(number) << '\n';
+    for (auto it : number) cout << it << ' ';
+
+    return 0;
+}
