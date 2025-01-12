671f4fa30e3f2a61757ac783ce40855a04e973d1
Fovir <fovir@disroot.org>
2025-01-12 18:06:18 +0800

    solve problem 70

---
+++ LeetCode/70.climbing-stairs/70.climbing-stairs.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=70 lang=cpp
+ *
+ * [70] Climbing Stairs
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        if (n < 3)
+            return n;
+
+        vector<int> methods = {0, 1, 2, 0};
+        methods.resize(46);
+
+        for (int i = 3; i <= n; i++)
+            methods[i] = methods[i - 1] + methods[i - 2];
+
+        return methods[n];
+    }
+};
+// @lc code=end
+
+int main(void)
+{
+    Solution sol;
+    int n;
+
+    while (cin >> n) cout << sol.climbStairs(n) << '\n';
+
+    return 0;
+}
