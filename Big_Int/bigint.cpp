459c656f4864b0aabb00d2f80c0bbc57b7fa9702
Fovir <fovir@disroot.org>
2025-02-20 10:09:55 +0800

    add + reload

--- Big_Int/bigint.cpp
+++ Big_Int/bigint.cpp
@@ -1,6 +1,10 @@
 #include "bigint.h"
+#include <algorithm>
 #include <cctype>
 #include <cmath>
+
+static int Char2Digit(const char & ch);
+static char Digit2Char(const int & digit);
 
 BigInt::BigInt(std::string & s) : number(s)
 {
@@ -9,7 +13,7 @@ BigInt::BigInt(std::string & s) : number(s)
 BigInt::BigInt(int num)
 {
     number = "";
-    bool is_negative = num < 0;
+    bool IsNegative = num < 0;
     num = std::abs(num);
 
     while (num)
@@ -18,8 +22,28 @@ BigInt::BigInt(int num)
         num /= 10;
     }
 
-    if (is_negative)
+    if (IsNegative)
         number.insert(number.begin(), '-');
+}
+
+BigInt BigInt::operator-() const
+{
+    BigInt temp = *this;
+
+    if (temp.IsNegative())
+        temp.number.erase(temp.number.begin());
+    else
+        temp.number.insert(temp.number.begin(), '-');
+
+    return temp;
+}
+
+BigInt BigInt::abs() const
+{
+    if (*this < BigInt(0))
+        return this->operator-();
+    else
+        return *this;
 }
 
 std::ostream & operator<<(std::ostream & os, const BigInt & bi)
@@ -37,32 +61,81 @@ std::istream & operator>>(std::istream & is, BigInt & bi)
     return is;
 }
 
-bool operator>(BigInt & first, BigInt & second)
+bool operator>(const BigInt & first, const BigInt & second)
 {
-    if (first.is_negative() ^ second.is_negative())
-        return !first.is_negative();
+    if (first.IsNegative() ^ second.IsNegative())
+        return !first.IsNegative();
 
-    int first_length = first.number.length(),
-        second_length = second.number.length();
+    int first_length = first.Length(), second_length = second.Length();
 
     if (first_length != second_length)
-        return (first.is_negative() ? first_length < second_length
-                                    : first_length > second_length);
+        return (first.IsNegative() ? first_length < second_length
+                                   : first_length > second_length);
 
-    for (int i = (first.is_negative() ? 1 : 0); i < first_length; i++)
+    for (int i = (first.IsNegative() ? 1 : 0); i < first_length; i++)
         if (first[i] != second[i])
-            return (first.is_negative() ? first[i] < second[i]
-                                        : first[i] > second[i]);
+            return (first.IsNegative() ? first[i] < second[i]
+                                       : first[i] > second[i]);
 
     return false;
 }
 
-bool operator==(BigInt & first, BigInt & second)
+bool operator==(const BigInt & first, const BigInt & second)
 {
     return first.number == second.number;
 }
 
-bool operator<(BigInt & first, BigInt & second)
+bool operator<(const BigInt & first, const BigInt & second)
 {
     return !(first > second || first == second);
+}
+
+BigInt operator+(const BigInt & fn, const BigInt & sn)
+{
+    if (fn.IsNegative() ^ sn.IsNegative())
+        return (fn.IsNegative() ? sn - (-fn) : fn - (-sn));
+
+    std::string first = fn.abs(), second = sn.abs(), result("");
+
+    std::reverse(first.begin(), first.end());
+    std::reverse(second.begin(), second.end());
+
+    (first.length() < second.length() ? first : second)
+        .append((first.length() < second.length()
+                     ? second.length() - first.length()
+                     : first.length() - second.length()),
+                '0');
+
+    int next = 0;
+    for (int i = 0; i < first.length(); i++)
+    {
+        int digit = (Char2Digit(first[i]) + Char2Digit(second[i]) + next);
+        next = digit / 10;
+        digit %= 10;
+        result += Digit2Char(digit);
+    }
+
+    if (next)
+        result += Digit2Char(next);
+
+    std::reverse(result.begin(), result.end());
+    if (fn.IsNegative())
+        result.insert(result.begin(), '-');
+
+    return result;
+}
+
+BigInt operator-(const BigInt & fn, const BigInt & sn)
+{
+    return BigInt(0);
+}
+
+int Char2Digit(const char & ch)
+{
+    return std::isdigit(ch) ? ch - '0' : ch;
+}
+
+char Digit2Char(const int & digit)
+{
+    return digit + '0';
 }
--- Big_Int/bigint.h
+++ Big_Int/bigint.h
@@ -1,6 +1,9 @@
 #ifndef _BIGINT_H_
 #define _BIGINT_H_
 
+#include <algorithm>
+#include <cctype>
+#include <cmath>
 #include <iostream>
 #include <string>
 
@@ -13,18 +16,26 @@ public:
     BigInt() : number("0") {}
     BigInt(std::string & s);
     BigInt(int num);
+    operator std::string() const { return number; }
 
-    bool is_negative() const { return number.length() > 0 && number[0] == '-'; }
+    bool IsNegative() const { return number.length() > 0 && number[0] == '-'; }
+    const int Length() const { return number.length(); }
 
     const char operator[](int i) const { return number[i]; }
     char operator[](int i) { return number[i]; }
 
+    BigInt operator-() const;
+    BigInt abs() const;
+
     friend std::ostream & operator<<(std::ostream & os, const BigInt & bi);
     friend std::istream & operator>>(std::istream & is, BigInt & bi);
 
-    friend bool operator>(BigInt & first, BigInt & second);
-    friend bool operator==(BigInt & first, BigInt & second);
-    friend bool operator<(BigInt & first, BigInt & second);
+    friend bool operator>(const BigInt & first, const BigInt & second);
+    friend bool operator==(const BigInt & first, const BigInt & second);
+    friend bool operator<(const BigInt & first, const BigInt & second);
+
+    friend BigInt operator+(const BigInt & fn, const BigInt & sn);
+    friend BigInt operator-(const BigInt & fn, const BigInt & sn);
 };
 
 #endif // !_BIGINT_H_
