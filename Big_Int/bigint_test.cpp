26ac66b792678f41cfdff186cb6e5b0db452fd24
Fovir <fovir@disroot.org>
2025-02-23 17:34:41 +0800

    test the bigint

---
+++ Big_Int/bigint_test.cpp
@@ -0,0 +1,12 @@
+#include "bigint.h"
+#include "bigint_math.hpp"
+#include <iostream>
+
+int main(void)
+{
+    BigInt n;
+
+    while (std::cin >> n) big_int_math::primeFactorize(n);
+
+    return 0;
+}
