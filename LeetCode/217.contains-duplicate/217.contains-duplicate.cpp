ce75de7c45021c3036bf2a12d0d8dc47fe973ba7
Fovir <fovir@disroot.org>
2025-03-22 18:29:12 +0800

    feat: solve 217

---
+++ LeetCode/217.contains-duplicate/217.contains-duplicate.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=217 lang=cpp
+ *
+ * [217] Contains Duplicate
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    bool containsDuplicate(vector<int> & nums)
+    {
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 1; i++)
+            if (nums[i] == nums[i + 1])
+                return true;
+
+        return false;
+    }
+};
+// @lc code=end
+
+int main(void)
+{
+    vector<int> nums = {1, 2, 3, 4};
+    Solution    sol;
+
+    cout << sol.containsDuplicate(nums) << '\n';
+
+    return 0;
+}
