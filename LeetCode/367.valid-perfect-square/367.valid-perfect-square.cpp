34d8316981d663a5d46016c955bc18289f2b8d5e
Fovir <fovir@disroot.org>
2025-04-21 13:46:45 +0800

    feat: solve 367

---
+++ LeetCode/367.valid-perfect-square/367.valid-perfect-square.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=367 lang=cpp
+ *
+ * [367] Valid Perfect Square
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    bool isPerfectSquare(int num)
+    {
+        int left = 1, right = num;
+
+        while (left <= right)
+        {
+            long long middle = left + (right - left) / 2;
+
+            if (middle * middle == num)
+                return true;
+
+            if (middle * middle > num)
+                right = middle - 1;
+            else
+                left = middle + 1;
+        }
+
+        return false;
+    }
+};
+// @lc code=end
+
+int main(void)
+{
+    int      n;
+    Solution sol;
+
+    while (cin >> n) cout << sol.isPerfectSquare(n) << '\n';
+
+    return 0;
+}
