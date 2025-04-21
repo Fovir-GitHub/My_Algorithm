de6b0c8b82d93a23625b08bb9332455e741ca4c3
Fovir <fovir@disroot.org>
2025-04-21 10:33:29 +0800

    feat: add another solution

--- LeetCode/345.reverse-vowels-of-a-string/345.reverse-vowels-of-a-string.cpp
+++ LeetCode/345.reverse-vowels-of-a-string/345.reverse-vowels-of-a-string.cpp
@@ -13,21 +13,35 @@ class Solution
 public:
     string reverseVowels(string s)
     {
-        string vowels = "aeiou";
+        // string vowels = "aeiou";
+        // int    left = 0, right = s.size() - 1;
+
+        // while (left < right)
+        // {
+        //     while (vowels.find(tolower(s[left])) == string::npos &&
+        //            left < right)
+        //         left++;
+
+        //     while (vowels.find(tolower(s[right])) == string::npos &&
+        //            right > left)
+        //         right--;
+
+        //     std::swap(s[left], s[right]);
+        //     left++, right--;
+        // }
+
+        // return s;
+
         int    left = 0, right = s.size() - 1;
+        string vowels = "aeiouAEIOU";
 
         while (left < right)
         {
-            while (vowels.find(tolower(s[left])) == string::npos &&
-                   left < right)
-                left++;
+            left  = s.find_first_of(vowels, left);
+            right = s.find_last_of(vowels, right);
 
-            while (vowels.find(tolower(s[right])) == string::npos &&
-                   right > left)
-                right--;
-
-            std::swap(s[left], s[right]);
-            left++, right--;
+            if (left < right)
+                swap(s[left++], s[right--]);
         }
 
         return s;
