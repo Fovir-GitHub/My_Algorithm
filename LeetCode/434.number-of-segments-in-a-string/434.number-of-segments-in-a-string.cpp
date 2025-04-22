251a08d8f3512d8044c2bd4d45dce1a551e66315
Fovir <fovir@disroot.org>
2025-04-22 09:16:48 +0800

    feat: solve 434

---
+++ LeetCode/434.number-of-segments-in-a-string/434.number-of-segments-in-a-string.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=434 lang=cpp
+ *
+ * [434] Number of Segments in a String
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    int countSegments(string s)
+    {
+        istringstream iss(s);
+        string        temp;
+        int           count = 0;
+
+        while (iss >> temp) count++;
+
+        return count;
+    }
+};
+// @lc code=end
+
+int main(void)
+{
+    Solution sol;
+    string   input;
+
+    while (getline(cin, input)) cout << sol.countSegments(input) << '\n';
+
+    return 0;
+}
