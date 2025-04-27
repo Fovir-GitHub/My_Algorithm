8db8ce33288fd63963a304aadc944daac3a5a48b
Fovir <fovir@disroot.org>
2025-04-27 20:11:38 +0800

    feat: solve 495

---
+++ LeetCode/495.teemo-attacking/495.teemo-attacking.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=495 lang=cpp
+ *
+ * [495] Teemo Attacking
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    int findPoisonedDuration(vector<int> & timeSeries, int duration)
+    {
+        return inner_product(timeSeries.begin() + 1, timeSeries.end(),
+                             timeSeries.begin(), duration, plus<>(),
+                             [duration](int current, int previous) {
+                                 return min(current - previous, duration);
+                             });
+    }
+};
+// @lc code=end
