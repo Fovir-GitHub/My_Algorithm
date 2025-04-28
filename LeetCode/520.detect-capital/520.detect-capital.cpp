1f77b6b55ca72695c6b519de148f0519d913a623
Fovir <fovir@disroot.org>
2025-04-28 14:26:09 +0800

    feat: solve 520

---
+++ LeetCode/520.detect-capital/520.detect-capital.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=520 lang=cpp
+ *
+ * [520] Detect Capital
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    bool detectCapitalUse(string word)
+    {
+        if (word.length() <= 1)
+            return true;
+
+        if (islower(word[0]))
+            return (find_if(word.begin(), word.end(), ::isupper) == word.end());
+        else if (isupper(word[1]))
+            return (find_if(word.begin(), word.end(), ::islower) == word.end());
+        else
+            return (find_if(word.begin() + 1, word.end(), ::isupper) ==
+                    word.end());
+    }
+};
+// @lc code=end
+
+int main(void)
+{
+    string   s;
+    Solution sol;
+
+    while (cin >> s) cout << sol.detectCapitalUse(s) << '\n';
+
+    return 0;
+}
