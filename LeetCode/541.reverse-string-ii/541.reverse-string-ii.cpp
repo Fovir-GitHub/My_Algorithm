ec27ba9c01fface67247481a9048e2754f945b06
Fovir <fovir@disroot.org>
2025-04-29 10:54:15 +0800

    feat: solve 541

---
+++ LeetCode/541.reverse-string-ii/541.reverse-string-ii.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=541 lang=cpp
+ *
+ * [541] Reverse String II
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    string reverseStr(string s, int k)
+    {
+        for (int i = 0; i < s.size(); i += 2 * k)
+            if (i + k <= s.size())
+                reverse(s.begin() + i, s.begin() + i + k);
+            else
+                reverse(s.begin() + i, s.end());
+
+        return s;
+    }
+};
+// @lc code=end
+
+int main(void)
+{
+    string   s("abcd");
+    int      k = 2;
+    Solution sol;
+
+    cout << sol.reverseStr(s, k) << '\n';
+
+    return 0;
+}
