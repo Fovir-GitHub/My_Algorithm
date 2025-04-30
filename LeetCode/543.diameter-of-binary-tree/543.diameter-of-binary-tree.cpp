050cb40847b53ffc0e7a08b30e3e1af4b8b3ec3a
Fovir <fovir@disroot.org>
2025-04-30 17:17:48 +0800

    feat: solve 543

---
+++ LeetCode/543.diameter-of-binary-tree/543.diameter-of-binary-tree.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=543 lang=cpp
+ *
+ * [543] Diameter of Binary Tree
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+
+// struct TreeNode
+// {
+//     int        val;
+//     TreeNode * left;
+//     TreeNode * right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode * left, TreeNode * right)
+//         : val(x)
+//         , left(left)
+//         , right(right)
+//     {
+//     }
+// };
+
+class Solution
+{
+private:
+    int result = 0;
+
+public:
+    int diameterOfBinaryTree(TreeNode * root)
+    {
+        dfs(root);
+
+        return result;
+    }
+
+    int dfs(TreeNode * node)
+    {
+        if (!node)
+            return 0;
+
+        int left = dfs(node->left), right = dfs(node->right);
+
+        result = max(result, left + right);
+
+        return 1 + max(left, right);
+    }
+};
+// @lc code=end
