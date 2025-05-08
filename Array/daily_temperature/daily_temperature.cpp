ab5c9f59417d7255cadb6334ec2fdb4420b26501
Fovir <fovir@disroot.org>
2025-05-08 16:54:45 +0800

    feat: Solve lab problems.

---
+++ Array/daily_temperature/daily_temperature.cpp
@@ -0,0 +1,125 @@
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <vector>
+
+using temperature_array = std::vector<std::vector<double>>;
+
+void inputDailyTemperature(temperature_array & ta);
+
+void displayDailyTemperature(const temperature_array & ta);
+
+void displayAverageTemperature(const temperature_array & ta);
+
+void showMaxAndMinTemperature(const temperature_array & ta);
+
+void displayWeeklyAverageTemperature(const temperature_array & ta);
+
+int main(void)
+{
+    const int DAYS = 7, ZONE = 5;
+    std::freopen("./input.in", "r", stdin);
+
+    // std::random_device               rd;
+    // std::mt19937                     gen(rd());
+    // std::uniform_real_distribution<> dis(20.0, 40.0);
+
+    // for (int i = 0; i < DAYS; i++)
+    // {
+    //     for (int j = 0; j < ZONE; j++)
+    //         std::cout << std::fixed << std::setprecision(2) << dis(gen) << '
+    //         ';
+    //     std::cout << '\n';
+    // }
+
+    temperature_array daily_temperature(DAYS, std::vector<double>(ZONE));
+
+    inputDailyTemperature(daily_temperature);
+    std::cout << '\n';
+
+    displayDailyTemperature(daily_temperature);
+    std::cout << '\n';
+
+    displayAverageTemperature(daily_temperature);
+    std::cout << '\n';
+
+    showMaxAndMinTemperature(daily_temperature);
+    std::cout << '\n';
+
+    displayWeeklyAverageTemperature(daily_temperature);
+    std::cout << '\n';
+
+    return 0;
+}
+
+void inputDailyTemperature(temperature_array & ta)
+{
+    for (int i = 0; i < ta.size(); i++)
+    {
+        std::cout << "Please input for day " << (i + 1) << ":\n";
+        for (int j = 0; j < ta[i].size(); j++)
+        {
+            std::cout << "Please input for Zone " << char(j + 'A') << ": ";
+            std::cin >> ta[i][j];
+        }
+    }
+
+    return;
+}
+
+void displayDailyTemperature(const temperature_array & ta)
+{
+    std::cout << '\t';
+    for (int i = 0; i < ta[0].size(); i++)
+        std::cout << "Zone " << char(i + 'A') << '\t';
+    std::cout << '\n';
+
+    for (int i = 0; i < ta.size(); i++)
+    {
+        std::cout << "Day " << (i + 1) << '\t';
+        for (int j = 0; j < ta[i].size(); j++)
+            std::cout << std::fixed << std::setprecision(2) << ta[i][j] << '\t';
+        std::cout << '\n';
+    }
+
+    return;
+}
+
+void displayAverageTemperature(const temperature_array & ta)
+{
+    for (int i = 0; i < ta.size(); i++)
+        std::cout << "Average Temperature of Day " << (i + 1) << ": "
+                  << std::fixed << std::setprecision(2)
+                  << std::accumulate(ta[i].begin(), ta[i].end(), double(0)) /
+                         ta[i].size()
+                  << '\n';
+
+    return;
+}
+
+void showMaxAndMinTemperature(const temperature_array & ta)
+{
+    std::cout << "\tHottest\tColdest\n";
+    for (int i = 0; i < ta.size(); i++)
+        std::cout << "Day " << (i + 1) << '\t'
+                  << (*std::max_element(ta[i].begin(), ta[i].end())) << '\t'
+                  << (*std::min_element(ta[i].begin(), ta[i].end())) << '\n';
+
+    return;
+}
+
+void displayWeeklyAverageTemperature(const temperature_array & ta)
+{
+    for (int i = 0; i < ta[0].size(); i++)
+    {
+        double sum = static_cast<double>(0);
+        for (int j = 0; j < ta.size(); j++) sum += ta[j][i];
+        std::cout << "Weekly Average Temperature for Zone "
+                  << static_cast<char>(i + 'A') << ": " << (sum / ta.size())
+                  << '\n';
+    }
+
+    return;
+}
---
+++ Array/seat_system/seat_system.cpp
@@ -0,0 +1,104 @@
+#include <bitset>
+#include <iostream>
+#include <vector>
+
+class SeatSystem
+{
+private:
+    enum { ROW = 10, COLUMN = 15 };
+    enum QUERY_RESULT { AVAILABLE = 0, BOOKED = 1, OUT_OF_RANGE = -1 };
+
+    std::vector<short> seat;
+
+public:
+    SeatSystem() : seat(std::vector<short>(ROW)) {}
+    ~SeatSystem() {}
+
+    void DisplaySeatingChart() const
+    {
+        for (const short & number : seat)
+        {
+            for (const char & bit : std::bitset<COLUMN>(number).to_string())
+                std::cout << (bit == '1' ? 'B' : 'A') << ' ';
+            std::cout << '\n';
+        }
+
+        return;
+    }
+
+    int GetSeatStatus(int row, int column) const
+    {
+        if (row < 0 || row >= ROW || column < 0 || column >= COLUMN)
+            return QUERY_RESULT::OUT_OF_RANGE;
+
+        return seat[row] & (1 << (COLUMN - column - 1))
+                   ? QUERY_RESULT::BOOKED
+                   : QUERY_RESULT::AVAILABLE;
+    }
+
+    void BookSeat(int row, int column)
+    {
+        int seat_status = GetSeatStatus(row, column);
+
+        if (seat_status == QUERY_RESULT::OUT_OF_RANGE)
+        {
+            std::cout << "Out of range!\n";
+            return;
+        }
+
+        if (seat_status)
+        {
+            std::cout << "Already booked!\n";
+            return;
+        }
+
+        seat[row] |= (1 << (COLUMN - column - 1));
+
+        return;
+    }
+
+    void CancelSeat(int row, int column)
+    {
+        if (GetSeatStatus(row, column) != QUERY_RESULT::BOOKED)
+        {
+            std::cout << "Error!\n";
+            return;
+        }
+
+        seat[row] &= ~(1 << (COLUMN - column - 1));
+
+        return;
+    }
+
+    void CheckAvailablility() const
+    {
+        int booked = 0;
+
+        for (const short & number : seat)
+            booked += std::bitset<COLUMN>(number).count();
+
+        std::cout << "Booked: " << booked << '\n'
+                  << "Available: " << (COLUMN * ROW - booked) << '\n';
+
+        return;
+    }
+
+    int SearchConsecutiveSeats(int n) const
+    {
+        if (n < 0 || n > COLUMN)
+            return QUERY_RESULT::OUT_OF_RANGE;
+
+        short mask = (1 << (n + 1)) - 1;
+
+        for (int i = 0; i < seat.size(); i++)
+            for (int j = 0; j < COLUMN - n; j++)
+                if (!(seat[i] & (mask << j)))
+                    return i;
+
+        return QUERY_RESULT::OUT_OF_RANGE;
+    }
+};
+
+int main(void)
+{
+}
