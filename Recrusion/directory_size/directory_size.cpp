2e374484f0bf877b5542912e5a08d1354741b9b0
Fovir <fovir@disroot.org>
2025-05-09 11:13:33 +0800

    feat: Solve problem.

---
+++ Recrusion/directory_size/directory_size.cpp
@@ -0,0 +1,31 @@
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+int getDirectorySize(const fs::path & path);
+
+int main(void)
+{
+    fs::path path;
+
+    while (std::cin >> path)
+        std::cout << "Size: " << getDirectorySize(path) << " B\n";
+
+    return 0;
+}
+
+int getDirectorySize(const fs::path & path)
+{
+    using Entry = fs::directory_entry;
+
+    int result = 0;
+
+    for (const Entry & entry : fs::directory_iterator(path))
+        if (entry.is_regular_file())
+            result += entry.file_size();
+        else
+            result += getDirectorySize(entry);
+
+    return result;
+}
